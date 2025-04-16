d0dd6cadd0e8dc438bfdbf3d1352d9b66617f256
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-04-16 12:27:26 +0200

    Begin opzet repo server side #1

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-console.log('Hier komt je server voor Sprint 10.')
+import express from 'express'
+import { Liquid } from 'liquidjs';
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
-console.log('Zet \'m op!')
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views')
+
+// Routes
+
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+    console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
