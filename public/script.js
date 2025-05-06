b92ec7600aea4df9bd6f35ddec32ff9b15979dd1
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-05-06 12:00:06 +0200

    Loading state loader begin #4
    Loader icoontje is er nog niet, maar voor de rest werkt die wel al goed. Dat word dus de volgende stap

--- public/script.js
+++ public/script.js
@@ -1,24 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const successMessage = document.querySelector(".succes-box");
-
-    if (successMessage && successMessage.classList.contains('active')) {
-        setTimeout(() => {
-            successMessage.classList.add('show');
-        }, 4);
     
-        setTimeout(() => {
-            successMessage.classList.remove('show');
-            successMessage.classList.add('fade-out');
-        }, 2400);
-    }
-    
-
     const closeBtn = document.querySelector(".close-btn");
     const details = document.querySelector("details");
 
     closeBtn.addEventListener('click', () => {
         details.removeAttribute('open')
     });
-
 });
-
