d16ed23d4869c6acb81687ea1777ebbd94333843
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-05-06 11:22:18 +0200

    Ingeving states #4
    De box blijft denk ik nog staan omdat ik twee keer de class show toevoeg. Ik ga even kijken of dit werkt zo.

--- public/script.js
+++ public/script.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const successMessage = document.querySelector(".succes-box");
 
-    if (successMessage && successMessage.classList.contains('show')) {
+    if (successMessage && successMessage.classList.contains('active')) {
         setTimeout(() => {
             successMessage.classList.add('show');
         }, 4);
-
+    
         setTimeout(() => {
+            successMessage.classList.remove('show');
             successMessage.classList.add('fade-out');
         }, 2400);
     }
+    
 
     const closeBtn = document.querySelector(".close-btn");
     const details = document.querySelector("details");
